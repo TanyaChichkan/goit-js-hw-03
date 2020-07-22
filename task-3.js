8e8967e92a0bf93553f41ff6a1fac44d93f992ed
TanyaChichkan <TanyaChichkan@users.noreply.github.com>
2020-07-22 22:59:57 +0300

    Update task-3.js

--- task-3.js
+++ task-3.js
@@ -39,3 +39,56 @@
 //         chelsy: 38,
 //     }),
 // ); // lux
+
+// or
+
+// const findBestEmployee = function (employees) {
+//   const values = Object.values(employees);
+//   //   console.log(values);
+//   let biggestNum = values[0];
+//   let key;
+
+//   for (let i = 0; i < values.length; i += 1) {
+//     if (values[i] > biggestNum) {
+//       biggestNum = values[i];
+//     }
+//   }
+//   //   console.log(biggestNum);
+
+//   const arr = Object.entries(employees);
+
+//   for (key of arr) {
+//     if (key.includes(biggestNum)) {
+//       return key[0];
+//     }
+//   }
+// };
+
+// console.log(
+//   findBestEmployee({
+//     ann: 29,
+//     david: 35,
+//     helen: 1,
+//     lorence: 99,
+//   })
+// );
+
+// // lorence
+
+// console.log(
+//   findBestEmployee({
+//     poly: 12,
+//     mango: 17,
+//     ajax: 4,
+//   })
+// );
+// // mango
+
+// console.log(
+//   findBestEmployee({
+//     lux: 147,
+//     david: 21,
+//     kiwi: 19,
+//     chelsy: 38,
+//   })
+// );
